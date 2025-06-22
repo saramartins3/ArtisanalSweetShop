ccf7d47bebf3da7f553880d104d3d14ad3089b00
saramartins3 <sara.martino@studenti.unina.it>
2025-06-22 11:18:30 +0200

    Create ImpiegatoEntity.java

---
+++ ArtisanalSweetShopping/ArtisanalSweetShopping/ArtisanalSweetShopping/src/main/java/com/example/ArtisanalSweetShopping/com/example/ArtisanalSweetShopping/Entity/ImpiegatoEntity.java
@@ -0,0 +1,59 @@
+package com.example.ArtisanalSweetShopping.Entity;
+
+public class ImpiegatoEntity {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+
+    public ImpiegatoEntity(int id, String nome, String cognome, String email, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
